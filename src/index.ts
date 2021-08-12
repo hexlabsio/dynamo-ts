c4d46442ac0baaa5526e0fd10662eb1c3d48a4a3
Chris Barbour <chris.barbour03@gmail.com>
2021-08-12 16:31:30 +0100

    added optionals

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { DynamoEntryDefinition, TableEntryDefinition } from './dynamoTable';
 export * from './dynamoTable';
 
 export function defineTable<
-  D extends DynamoEntryDefinition,
+  D extends DynamoEntryDefinition['definition'],
   H extends keyof D,
   R extends keyof D | null = null,
   G extends Record<
