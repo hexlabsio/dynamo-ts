4299795e6cd72c088d27a1b3c203d93cb64580dc
Chris Barbour <chris.barbour@hexlabs.io>
2022-01-30 19:24:28 +0000

    added batch get item

--- src/dynamo-getter.ts
+++ src/dynamo-getter.ts
@@ -8,8 +8,10 @@ import {
 } from "./type-mapping";
 import {DynamoClientConfig} from "./dynamo-client-config";
 import {AttributeBuilder} from "./naming";
+import ConsumedCapacity = DocumentClient.ConsumedCapacity;
+import BatchGetItemInput = DocumentClient.BatchGetItemInput;
 
-export type GetItemExtras = Omit<GetItemInput, 'TableName' | 'Key'>;
+export type GetItemExtras = Pick<GetItemInput, 'ConsistentRead' | 'ReturnConsumedCapacity' | 'ProjectionExpression' | 'ExpressionAttributeNames'>;
 
 export class ProjectionHandler {
   static projectionFor<DEFINITION>(attributeBuilder: AttributeBuilder, definition: DEFINITION, expression?: string): [AttributeBuilder, string]{
@@ -34,7 +36,7 @@ export class DynamoGetter {
       config: DynamoClientConfig<DEFINITION>,
       key: DynamoKeysFrom<DEFINITION, HASH, RANGE>,
       options: GetItemExtras = {}
-  ) : Promise<DynamoClientConfig<DEFINITION>['tableType'] | undefined> {
+  ) : Promise<{item: DynamoClientConfig<DEFINITION>['tableType'] | undefined, consumedCapacity?: ConsumedCapacity}> {
     const [attributes, projection] = ProjectionHandler.projectionFor(AttributeBuilder.create(), config.definition, options.ProjectionExpression);
     const getInput: GetItemInput = {
       TableName: config.tableName,
@@ -49,7 +51,35 @@ export class DynamoGetter {
     const result = await config.client
         .get(getInput)
         .promise();
-    return result.Item as DynamoClientConfig<DEFINITION>['tableType'] | undefined;
+    return {item: result.Item as DynamoClientConfig<DEFINITION>['tableType'] | undefined, consumedCapacity: result.ConsumedCapacity};
+  }
+
+  async batchGet<
+      DEFINITION extends DynamoMapDefinition,
+      HASH extends keyof DynamoEntry<DEFINITION>,
+      RANGE extends DynamoRangeKey<DEFINITION,HASH>
+    >(
+        config: DynamoClientConfig<DEFINITION>,
+        keys: DynamoKeysFrom<DEFINITION, HASH, RANGE>[],
+        returnConsumedCapacity?: BatchGetItemInput['ReturnConsumedCapacity'],
+        consistent?: boolean
+  ): Promise<{items: DynamoClientConfig<DEFINITION>['tableType'][], consumedCapacity?: ConsumedCapacity}>{
+
+    const [attributes, projection] = ProjectionHandler.projectionFor(AttributeBuilder.create(), config.definition);
+    const batchGetInput: BatchGetItemInput = {
+      ReturnConsumedCapacity: returnConsumedCapacity,
+      RequestItems: { [config.tableName]: {
+        ...attributes.asInput(),
+        Keys: keys,
+          ...(projection ? {ProjectionExpression: projection} : {}),
+          ...((consistent !== undefined) ? {ConsistentRead: consistent}: {})
+      }}
+    }
+    if(config.logStatements) {
+      console.log(`BatchGetItemInput: ${JSON.stringify(batchGetInput, null, 2)}`)
+    }
+    const result = await config.client.batchGet(batchGetInput).promise();
+    return result.Responses![config.tableName] as any;
   }
 
 }
